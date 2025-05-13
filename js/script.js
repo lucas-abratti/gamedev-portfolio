d34e55965521bd6c8e0fd67d7b0ac4335c1fe49e
Lucas Abratti <lucasabratti99@gmail.com>
2025-05-13 12:13:20 -0300

    first commit v1.0

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Example: Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// You can also dynamically load project data from a JSON file if you prefer.
